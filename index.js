884849f3137fe9bc8895ba68e53c362635fab31a
Tanzeelakhan42 <tanzeelakhan42@gmail.com>
2026-02-25 02:27:13 +0530

    Log incoming HTTP requests to log.txt file using fs module

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http = require("http");
+const fs = require("fs"); //Creating a webServer using fs module i.e make log of incoming requests
 const myServer = http.createServer((req, res) => {
-  console.log("New request received");
-  res.end("Hello from server");
+  const log = `${Date.now()}:New Req Received\n`;
+  fs.appendFile("log.txt", log, (err, data) => {});
 });
 myServer.listen(8000, () => {
   console.log("Server started");
