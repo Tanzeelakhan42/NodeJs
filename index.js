4d9a572075b62e9bd4e746f2c2cbd51d67cb7d9b
Tanzeelakhan42 <tanzeelakhan42@gmail.com>
2026-02-25 02:35:05 +0530

    Routed HTTP requests based on URL paths

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const http = require("http");
 const fs = require("fs"); //Creating a webServer using fs module i.e make log of incoming requests
 const myServer = http.createServer((req, res) => {
-  const log = `${Date.now()}:New Req Received\n`;
-  fs.appendFile("log.txt", log, (err, data) => {});
+  const log = `${Date.now()}:${req.url}:New Req Received\n`; //check path of http requests
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (req.url) {
+      case "/":
+        res.end("HomePage");
+        break;
+      case "/about":
+        res.end("I am Tanzeela");
+        break;
+      default:
+        res.end("404 not found");
+    }
+  });
 });
 myServer.listen(8000, () => {
   console.log("Server started");
