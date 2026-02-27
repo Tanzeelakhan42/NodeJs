4fb2a14e78d4ea9d270e9b7fc9d7f01455d42ef8
Tanzeelakhan42 <tanzeelakhan42@gmail.com>
2026-02-27 16:25:54 +0530

    Parse incoming request URLs for better request handling

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const url = require("url");
 const myServer = http.createServer((req, res) => {
   if (req.url === "/favicon.ico") return res.end();
   const log = `${Date.now()}: ${req.url}: New request received\n`;
+  const myUrl = url.parse(req.url);
+  console.log(myUrl);
+
   fs.appendFile("log.txt", log, (err, data) => {
     switch (req.url) {
       case "/":
