5af56ec3d9442344bfbad3363eda50413f14fa8d
Tanzeelakhan42 <tanzeelakhan42@gmail.com>
2026-02-27 16:16:23 +0530

    Implement basic URL routing using switch cases with HTTP  request logging

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const http = require("http");
 const fs = require("fs");
 const myServer = http.createServer((req, res) => {
-  const log = `${Date.now()}: New request received\n`;
+  const log = `${Date.now()}: ${req.url}: New request received\n`;
   fs.appendFile("log.txt", log, (err, data) => {
+    switch (req.url) {
+      case "/":
+        res.end("HomePage");
+        break;
+      case "/about":
+        res.end("I am Tanzeela");
+        break;
+      default:
+        res.end("404 not found");
+    }
     console.log("New request received");
-    res.end("Hello from server");
   });
 });
 myServer.listen(9000, () => {
   console.log("Server started");
 });
+//
+//Implement basic URL routing using switch case
