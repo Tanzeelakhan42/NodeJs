a378cc9608b17c36524c4b536f2b4ccc84e58558
Tanzeelakhan42 <tanzeelakhan42@gmail.com>
2026-02-27 15:54:54 +0530

    Create basic HTTP server & listen on port 9000

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require("http");
+const myServer = http.createServer((req, res) => {
+  console.log("New request received");
+  res.end("Hello from server");
+});
+myServer.listen(9000, () => {
+  console.log("Server started");
+});
