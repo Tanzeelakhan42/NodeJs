5c3a558fbd8e562bbc1a48f88767e6aa39c9cb95
Tanzeelakhan42 <tanzeelakhan42@gmail.com>
2026-02-27 16:08:21 +0530

    Append incoming HTTP request logs to log.txt using fs module

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const http = require("http");
+const fs = require("fs");
 const myServer = http.createServer((req, res) => {
-  console.log("New request received");
-  res.end("Hello from server");
+  const log = `${Date.now()}: New request received\n`;
+  fs.appendFile("log.txt", log, (err, data) => {
+    console.log("New request received");
+    res.end("Hello from server");
+  });
 });
 myServer.listen(9000, () => {
   console.log("Server started");
