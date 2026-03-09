227cc9abd565013f3a7f4e0cacb4070a55a6460b
Tanzeelakhan42 <tanzeelakhan42@gmail.com>
2026-03-09 16:32:25 +0530

    create basic HTTP server with URL parsing, routing and request logging

---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+const myServer = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return res.end();
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
+  const log = `${Date.now()}: ${req.url}: New request received\n`;
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (myUrl.pathname) {
+      case "/":
+        res.end("HomePage");
+        break;
+      case "/about":
+        const userName = myUrl.query.myname;
+        res.end(`Hello from ${userName}`);
+        break;
+      case "/search":
+        const search = myUrl.query.search_query;
+        res.end(`Here are your results for ${search}`);
+        break;
+      default:
+        res.end("404 not found");
+    }
+    console.log("New request received");
+  });
+});
+myServer.listen(9000, () => {
+  console.log("Server started");
+});
